11ed3b64b4f50cb3e7af672dafdc803d8361d687
Brandon <phizzl3@gmail.com>
2021-01-19 10:13:27 -0600

    Initial commit - 01.19.2021

---
+++ xlclass.py
@@ -0,0 +1,501 @@
+# 01.19.2021
+"""
+Class for working with *.xlsx files using the Openpyxl module. 
+...Mainly for my job...
+"""
+
+
+import csv
+
+import openpyxl
+from openpyxl.styles import Font, PatternFill
+
+# Color dict for background fill
+COLORS = {
+    'red': PatternFill(fgColor='FF0000', fill_type='solid'),
+    'green': PatternFill(fgColor='00b050', fill_type='solid'),
+    'orange': PatternFill(fgColor='FFC000', fill_type='solid'),
+    'yellow': PatternFill(fgColor='FFFF00', fill_type='solid'),
+    'gray': PatternFill(fgColor='C0C0C0', fill_type='solid')
+}
+
+
+class Xlsx:
+    """
+    Class for working with Excel *.xlsx files using Openpyxl. 
+    Generates an Xlsx object with Openpyxl Workbook/Worksheet objects 
+    as attributes for use with the enclosed methods.
+    """
+
+    def __init__(self, filepath=None, sheetname=None):
+        """
+        Initialize main attributes for Xlsx objects if Path points to 
+        an existing Excel file. Creates a blank Workbook/Worksheet object 
+        if no filepath is passed. If multiple sheets are present in passed 
+        Excel file, the name of the sheet you want to work with can be passed 
+        as a string to 'sheetname' or you can select needed sheet from a menu.
+
+        Attrs:
+            *.wb (openpyxl.Workbook): Workbook object for Excel file.
+            *.ws (openpyxl.Workbook.worksheet): Active sheet for Excel file. 
+
+        Args:
+            filepath (str/pathlib.Path, optional): str/Path object representing 
+                    *.xlsx input file.
+            sheet (str, optional): Name representing which sheet you want to 
+                    work with. ex: 'Invoice'
+        """
+        if filepath:
+            self.wb = openpyxl.load_workbook(filepath)
+            if len(self.wb.sheetnames) == 1:
+                self.ws = self.wb.active
+            else:
+                if sheetname:
+                    self.ws = self.wb[sheetname]
+                else:
+                    # Display availible sheets and set worksheet based on selection
+                    print('\n Which tab/worksheet are we using?\n')
+                    for num, sheet in enumerate(self.wb.sheetnames, 1):
+                        print(f' {num}: {sheet}')
+                    while True:
+                        try:
+                            self.ws = self.wb[self.wb.sheetnames[int(
+                                input('\n Selection: '))-1]]
+                            break
+                        except ValueError:
+                            print('\n Try again...')
+
+        else:
+            self.wb = openpyxl.Workbook()
+            self.ws = self.wb.active
+
+    def copy_sheet_data(self, other, columns):
+        """
+        Copy cell values from source Excel Worksheet to target Excel Worksheet 
+        using a passed dictionary of column letters.
+
+        Args:
+            other (Xlsx): Input Xlsx Excel file object to copy values from.
+            columns (dict{str: str}): Dictionary of column letters representing the source and 
+                    target column letters to copy the values. ex: {'A': 'C', 'D': 'B'}
+
+        Returns:
+            self
+        """
+        try:
+            for row, _cell in enumerate(other.ws['A'], 1):
+                for scol, tcol in columns.items():
+                    self.ws[f'{tcol.upper()}{row}'] = other.ws[f'{scol.upper()}{row}'].value
+
+        except Exception as e:
+            print(f"\nError - copy_sheet_data: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def copy_csv_data(self, incsv):
+        """
+        Copy all values from csv file to target Excel Worksheet.
+
+        Args:
+            incsv (pathlib.Path): Path object representing a csv file. 
+
+        Returns:
+            self
+        """
+        try:
+            with open(incsv, 'r') as f:
+                reader = csv.reader(f)
+                [self.ws.append(row) for row in reader]
+
+        except Exception as e:
+            print(f"\nError - copy_csv_data: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def sort_and_replace(self, sortcol, startrow=1):
+        """
+        Sort and replace cell values based on values of a specific column. 
+        Use this BEFORE any cell formatting, etc as it DELETES the values and then 
+        replaces them after sorting.
+
+        Args:
+            sortcol (str): Column letter containing the values to use as "keys" to 
+                    sort row data by. ex: 'A'
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            sortme = []
+            for row, rowdata in enumerate(self.ws.iter_rows(values_only=True), 1):
+                if row >= startrow:
+                    sortme.append([str(self.ws[f'{sortcol.upper()}{row}'].value).lower(), ])
+                    sortme[row-startrow].append(rowdata)
+
+            self.ws.delete_rows(startrow, self.ws.max_row)
+
+            for sortval, rowvalues in sorted(sortme):
+                self.ws.append(rowvalues)
+
+        except Exception as e:
+            print(f"\nError - sort_and_replace: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def name_headers(self, headers, hdrrow=1, bold=False):
+        """
+        Cycle through header row and fill cells with values.
+
+        Args:
+            headers (dict{str:str}): Str pairs of columns and header name values. 
+                    ex: {'A': 'Name'}
+            hdrrow (int, optional): Row to be used for headers. Defaults to 1.
+            bold (bool, optional): Option to bold values in headers. Defaults to False.
+
+        Returns:
+            self
+        """
+        try:
+            for col, name in headers.items():
+                self.ws[f'{col.upper()}{hdrrow}'] = name
+            if bold:
+                for each in self.ws[f'{hdrrow}:{hdrrow}']:
+                    each.font = Font(bold=True)
+
+        except Exception as e:
+            print(f"\nError - name_headers: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def get_matching_value(self, srchcol, srchval, retcol, startrow=1):
+        """
+        Search column for a value and return the corresponding value 
+        from another column in the same row.
+
+        Args:
+            srchcol (str): Column letter to search for a value. ex: 'A'
+            srchval (str): Value to search column for. ex: 'Total'
+            retcol (str): Column letter containing the corresponding value 
+                   to be returned. ex: 'B'
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            str: Value from corresponding cell in the same row as search value. 
+                 Returns False if value search value is not found. 
+        """
+        try:
+            for row, cell in enumerate(self.ws[srchcol.upper()], 1):
+                if row >= startrow and cell.value:
+                    if srchval in str(cell.value):
+                        return self.ws[f'{retcol.upper()}{row}'].value
+
+        except Exception as e:
+            print(f"\nError - get_matching_value: {e}")
+            input("[ENTER] to continue...")
+
+        return False
+
+    def set_matching_value(self, srchcol, srchval, trgtcol, setval, startrow=1):
+        """
+        Search column for a value and set a corresponding value in another 
+        column in the same row. 
+
+        Args:
+            srchcol (str): Column letter to search for a value. ex: 'A'
+            srchval (str): Value to search column for. ex: 'Total'
+            trgtcol (str): Column letter to set with corresponding value. ex: 'B'
+            setval (str): Value to insert into target cell.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            for row, cell in enumerate(self.ws[srchcol.upper()], 1):
+                if row >= startrow and cell.value:
+                    if srchval in str(cell.value):
+                        self.ws[f'{trgtcol.upper()}{row}'] = setval
+
+        except Exception as e:
+            print(f"\nError - set_matching_value: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def find_remove_row(self, col, srch, startrow=1):
+        """
+        Remove row based on a specific value found in a column.
+
+        Args:
+            col (str): Column letter to search for the needed value.
+            srch (str): Value to search for.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        row = startrow
+        try:
+            while row <= self.ws.max_row:
+                if self.ws[f'{col.upper()}{row}'].value:
+                    if srch in str(self.ws[f'{col.upper()}{row}'].value):
+                        self.ws.delete_rows(row, 1)
+                    else:
+                        row += 1
+                else:
+                    row += 1
+
+        except Exception as e:
+            print(f"\nError - find_remove_row: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def find_replace(self, col, fndrplc, skip=None, startrow=1):
+        """
+        Search column for a string value and replace it the value is 
+        not listed in 'skip'.
+
+        Args:
+            col (str): Column letter to search for the needed values.
+            fndrplc (dict{str: str}): Dictionary of pairs to find and replace. 
+                    ex: {'find': 'replace'}.
+            skip (list(str), optional): List of values to ignore when replacing.
+                 Defaults to None.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            if not skip:
+                skip = []
+            for row, cell in enumerate(self.ws[col.upper()], 1):
+                if row >= startrow:
+                    if cell.value and str(cell.value).lower() not in skip:
+                        for find, replace in fndrplc.items():
+                            if find in str(cell.value):
+                                self.ws[f'{col.upper()}{row}'] = cell.value.replace(
+                                    find, replace)
+
+        except Exception as e:
+            print(f"\nError - find_replace: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def move_values(self, scol, tcol, vals, startrow=1):
+        """
+        Search source column for passed list of values and 
+        move them to target column. 
+
+        Args:
+            scol (str): Source column letter to search for values. ex: 'A'
+            tcol (str): Target column letter to move located values to. ex: 'B'
+            vals (list(str)): List of str values to move. ex: ('name', '20')
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            for row, cell in enumerate(self.ws[scol.upper()], 1):
+                if cell.value and row >= startrow:
+                    for item in vals:
+                        if item in str(cell.value):
+                            self.ws[f'{tcol.upper()}{row}'] = item
+                            self.ws[f'{scol.upper()}{row}'] = cell.value.replace(
+                                item, '')
+                            break
+
+        except Exception as e:
+            print(f"\nError - move_values: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def verify_length(self, col, length, fillcolor, skip=None, startrow=1):
+        """
+        Cycle through values in a column to verify their length marking 
+        cells of an incorrect length with a background fill color. 
+
+        Args:
+            col (str): Column to search for values. ex: 'B'
+            length (int): Total character length for correct values.
+            fillcolor (str): Background fill color selection from COLORS dict.
+            skip (list(str), optional): List of string values to skip when evaluating.
+                 Defaults to None.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            if not skip:
+                skip = []
+            if COLORS.get(fillcolor):
+                for row, cell in enumerate(self.ws[col.upper()], 1):
+                    if row >= startrow:
+                        if cell.value and cell.value.lower() not in skip:
+                            if len(cell.value) != length:
+                                self.ws[f'{col.upper()}{row}'].fill = COLORS.get(
+                                    fillcolor)
+            else:
+                print(f" Color '{fillcolor}' not available.")
+
+        except Exception as e:
+            print(f"\nError - verify_length: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def highlight_rows(self, col, srch, fillcolor, startrow=1):
+        """
+        Search row for specified str value and fill entire row 
+        with specified background fill color when found. 
+
+        Args:
+            col (str): Column to search for value. ex: 'B'
+            srch (str): Str value to search cells for.
+            fillcolor (str): Background fill color selection from COLORS dict.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            if COLORS.get(fillcolor):
+                for row, cell in enumerate(self.ws[col.upper()], 1):
+                    if row >= startrow:
+                        if cell.value and srch.lower() in str(cell.value).lower():
+                            for each in self.ws[f'{row}:{row}']:
+                                each.fill = COLORS.get(fillcolor)
+            else:
+                print(f" Color '{fillcolor}' not available.")
+
+        except Exception as e:
+            print(f"\nError - highlight_rows: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def number_type_fix(self, col, numtype, startrow=1):
+        """
+        Quick fix for cells that contain numbers formatted as 
+        text/str data. Cycle through cells replacing str formatted 
+        values with int/float values.
+
+        Args:
+            col (str): Column containing data to convert
+            numtype (str): 'i' or 'f' indicating which type of number 
+                    values the column contains (int/float)
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            for row, cell in enumerate(self.ws[col.upper()], 1):
+                if cell.value and row >= startrow:
+                    if numtype.lower() == 'i':
+                        self.ws[f'{col.upper()}{row}'] = int(cell.value)
+                    if numtype.lower() == 'f':
+                        self.ws[f'{col.upper()}{row}'] = float(cell.value)
+
+        except Exception as e:
+            print(f"\nError - number_type_fix: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def format_date(self, col, startrow=1):
+        """
+        Format str date value to (MM/DD/YYYY).
+
+        Args:
+            col (str): Column containing date values.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            for row, cell in enumerate(self.ws[col.upper()], 1):
+                if row >= startrow and cell.value:
+                    self.ws[f'{col.upper()}{row}'] = cell.value.strftime(
+                        '%m/%d/%Y')
+
+        except Exception as e:
+            print(f"\nError - format_date: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def format_currency(self, col, startrow=1):
+        """
+        Format str currency value to ($0,000.00).
+
+        Args:
+            col (str): Column containing currency values.
+            startrow (int, optional): Starting row number where values begin. 
+                     Defaults to 1.
+
+        Returns:
+            self
+        """
+        try:
+            for row, cell in enumerate(self.ws[col.upper()], 1):
+                if row >= startrow and cell.value:
+                    cell.number_format = '$#,###.00'
+
+        except Exception as e:
+            print(f"\nError - format_currency: {e}")
+            input("[ENTER] to continue...")
+
+        return self
+
+    def set_cell_size(self, pairs):
+        """
+        Selects rows and columns and adjusts their sizes using a dictionary 
+        of pairs of rows or columns along with corresponding height or width 
+        to adjust the size of cells from each pair. If dict key is type: str, 
+        adjusts column width. If dict key is type: int, adjusts row height. 
+
+        Args:
+            pairs (dict): Dictionary of column/row keys with target size  
+                  as their values.
+
+        Returns:
+            self
+        """
+        try:
+            for target, size in pairs.items():
+                if type(target) == str:
+                    self.ws.column_dimensions[target.upper()].width = size
+                elif type(target) == int:
+                    self.ws.row_dimensions[target].height = size
+                else:
+                    print(
+                        f"Invalid data pair. Check your info. {target: size}")
+                    input("[ENTER] to continue...")
+
+        except Exception as e:
+            print(f"\nError - set_cell_size: {e}")
+            input("[ENTER] to continue...")
+
+        return self
