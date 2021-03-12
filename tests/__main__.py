52094eb25e41d277a31b802bb91596e01792292a
Brandon <phizzl3@gmail.com>
2021-03-12 16:36:20 -0600

    Adding tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,3 +1,109 @@
-from .xlclass_tests import run_tests
+"""
+Run from top-level folder as module: 
 
-if __name__ == '__main__': run_tests()
+$ python3 -m tests
+"""
+
+import unittest
+from pathlib import Path
+
+from xlclass import Xlsx
+
+test_file = Path(__file__).resolve().parent / "_test.xlsx"
+
+
+class TestXlsx(unittest.TestCase):
+
+    def setUp(self):
+        # Add an Xlsx object as an attribute
+        self.xl = Xlsx(test_file)
+        # Generate a list of original contents to reset when needed
+        self.data = self.xl.generate_list()
+
+    def test_path(self):  #TODO: Move this to next function
+
+        self.assertTrue(str(self.xl.path).endswith("_test.xlsx"))
+        
+    def test_init(self):
+        # xls conversion
+        # xlsx path
+        pass
+    
+    def test_copy_sheet_data(self):
+        # check cell data match
+        pass
+    
+    def test_copy_csv_data(self):
+        # check cell data match
+        pass
+    
+    def test_sort_and_replace(self):
+        # add random entry and sort and check cell values
+        pass
+    
+    def test_name_headers(self):
+        # name and check values
+        pass
+    
+    def test_get_matching_value(self):
+        # get and check value
+        self.assertTrue(self.xl.get_matching_value('a', 'M', 'D') == 13000)
+    
+    def test_set_matching_value(self):
+        # set and check value
+        pass
+    
+    def test_find_remove_row(self):
+        # remove and then search for value
+        pass
+    
+    def test_find_replace(self):
+        # replace and verify value
+        pass
+    
+    def test_move_values(self):
+        # move and check value
+        pass
+    
+    def test_verify_length(self):
+        # check lengths
+        pass
+    
+    def test_highlight_rows(self):
+        # not sure unless i save out and check
+        pass
+    
+    def test_number_type_fix(self):
+        # fix and read in and verify value
+        pass
+    
+    def test_format_date(self):
+        # fix and read in and verify value
+        pass
+    
+    def test_format_currency(self):
+        # fix and read in and verify value
+        pass
+    
+    def test_set_cell_size(self):
+        # set and verify on save out
+        pass
+    
+    def test_save(self):
+        # save out and verify path exists
+        pass
+    
+    def test_generate_dictionary(self):
+        # generate and check values
+        pass
+    
+    def test_generate_list(self):
+        # generate and check values
+        pass
+    
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
