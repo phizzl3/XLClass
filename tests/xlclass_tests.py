edf48ef756cc157680cde0d8ccdcf7921074fc5d
phizzl3 <phizzl3@gmail.com>
2021-03-12 12:00:08 -0600

    Add previous tests

--- tests/xlclass_tests.py
+++ tests/xlclass_tests.py
@@ -1,14 +1,24 @@
+import unittest
+from pathlib import Path
+
 from xlclass import Xlsx
 
+test_file = Path(__file__).resolve().parent / "_test.xlsx"
 
 
+class TestXlsx(unittest.TestCase):
+
+    def setUp(self):
+        self.xl = Xlsx(test_file)
+
+    def test_print(self):
+
+        print('running')
+        self.assertTrue(str(self.xl.path).endswith("_test.xlsx"))
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 def run_tests():
