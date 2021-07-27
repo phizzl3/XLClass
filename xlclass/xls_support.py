ade40d0110d6626a1d10e0aff77d6e3e5f21979d
phizzl3 <phizzl3@gmail.com>
2021-07-27 00:41:47 +0000

    Update xls_support.py

--- xlclass/xls_support.py
+++ xlclass/xls_support.py
@@ -1,27 +1,28 @@
 import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
+import openpyxl
 
 
-def conver_xls()
+def conver_xls(obj, filepath, sheetname):
 
-# Convert xls to xlsx data using Pandas/Xlrd
-            if str(filepath).endswith(".xls"):
-                try:
-                    # Read data from xls and create xlsx object
-                    df = pd.read_excel(filepath, sheet_name=sheetname)
-                    self.path = filepath
-                    self.wb = openpyxl.Workbook()
-                    self.ws = self.wb.active
-                    self.ws.title = sheetname
-                    # Copy row data from xls to new xlsx object
-                    for row in dataframe_to_rows(df):
-                        self.ws.append(row)
-                    # Remove index row/colum created by Pandas
-                    self.ws.delete_cols(1, 1)
-                    self.ws.delete_rows(1, 1)
+    # Convert xls to xlsx data using Pandas/Xlrd
+    if not filepath:
+        input("Error converting from xls.\nBe sure to include the sheetname "
+              "when passing your filepath.")
+        exit("Exiting...")
+        
+        # Read data from xls and create xlsx object
+        df = pd.read_excel(filepath, sheet_name=sheetname)
+        obj.path = filepath
+        obj.wb = openpyxl.Workbook()
+        obj.ws = obj.wb.active
+        obj.ws.title = sheetname
+        
+        # Copy row data from xls to new xlsx object
+        for row in dataframe_to_rows(df):
+            obj.ws.append(row)
+                    
+        # Remove index row/colum created by Pandas
+        obj.ws.delete_cols(1, 1)
+        obj.ws.delete_rows(1, 1)
 
-                except Exception as e:
-                    print(f"\n Error: {e}\n Error converting from xls be sure "
-                          "to include sheetname argument when passing file.")
-                    input(" \"sheetname='Invoice'\", etc\n ENTER to close...")
-                    exit(" Exiting...")
