0957eee4ef6e55d33c663b8289c7ecfb688456c4
Brandon <phizzl3@gmail.com>
2021-07-27 09:15:55 -0500

    Update xls_support.py Update docs.

--- xlclass/xls_support.py
+++ xlclass/xls_support.py
@@ -1,3 +1,12 @@
+"""
+Adds xls conversion support using pandas and xlrd.
+
+Requirements:
+
+pandas==1.2.2
+xlrd==2.0.1
+
+"""
 import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
 import openpyxl
@@ -10,19 +19,18 @@ def convert_xls(obj, filepath=None, sheetname=None):
         input("Error converting from xls.\nBe sure to include the sheetname "
               "when passing your filepath.")
         exit("Exiting...")
-        
+
     # Read data from xls and create xlsx object
     df = pd.read_excel(filepath, sheet_name=sheetname)
     obj.path = filepath
     obj.wb = openpyxl.Workbook()
     obj.ws = obj.wb.active
     obj.ws.title = sheetname
-    
+
     # Copy row data from xls to new xlsx object
     for row in dataframe_to_rows(df):
         obj.ws.append(row)
-                
+
     # Remove index row/colum created by Pandas
     obj.ws.delete_cols(1, 1)
     obj.ws.delete_rows(1, 1)
-
